394b39e3697d407bb762d1b2b20374d2c3fb4d43
creativesayeedd-png <creativesayeedd@gmail.com>
2026-08-03 22:06:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -156,3 +156,34 @@ button.style.transform="translate(0,0)";
 });
 
 });
+/* ===============================
+   SCROLL TO TOP
+================================= */
+
+const scrollBtn=document.getElementById("scrollTop");
+
+window.addEventListener("scroll",()=>{
+
+if(window.scrollY>350){
+
+scrollBtn.classList.add("show");
+
+}else{
+
+scrollBtn.classList.remove("show");
+
+}
+
+});
+
+scrollBtn.addEventListener("click",()=>{
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+});
