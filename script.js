91ae527d875abad0223e0e473755bc581e0d971c
creativesayeedd-png <creativesayeedd@gmail.com>
2026-08-03 20:55:17 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,123 @@
+/* ==========================
+   LOADER
+========================== */
+
+window.addEventListener("load", () => {
+
+    const loader = document.getElementById("loader");
+
+    setTimeout(() => {
+
+        loader.style.opacity = "0";
+        loader.style.visibility = "hidden";
+
+    }, 800);
+
+});
+
+
+/* ==========================
+   STICKY HEADER
+========================== */
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 50) {
+
+        header.classList.add("scrolled");
+
+    } else {
+
+        header.classList.remove("scrolled");
+
+    }
+
+});
+
+
+/* ==========================
+   MOBILE MENU
+========================== */
+
+const menuBtn = document.querySelector(".menu-btn");
+
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+});
+
+
+/* ==========================
+   CLOSE MENU
+========================== */
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navLinks.classList.remove("active");
+
+    });
+
+});
+
+
+/* ==========================
+   ACTIVE MENU
+========================== */
+
+const sections = document.querySelectorAll("section");
+
+const navItems = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 120;
+
+        const sectionHeight = section.clientHeight;
+
+        if (scrollY >= sectionTop) {
+
+            current = section.getAttribute("id");
+
+        }
+
+    });
+
+    navItems.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+
+            link.classList.add("active");
+
+        }
+
+    });
+
+});
+
+
+/* ==========================
+   AOS
+========================== */
+
+AOS.init({
+
+    duration:1000,
+
+    once:true,
+
+    offset:80
+
+});
